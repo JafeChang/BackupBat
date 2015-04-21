65d2ca37fad735d830a9c4ac3baa90ebeb090b18
JafeChang <jafechang@gmail.com>
2015-04-21 19:56:57 +0800

    stupid again
    ...

--- BackupBat/info.jafe/info/jafe/BackupBat.java
+++ BackupBat/info.jafe/info/jafe/BackupBat.java
@@ -38,6 +38,7 @@ class Index extends JFrame{
 				(int) (0.5 * screenSize.y - 300), 800, 600);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.add(panel);
+		
 		this.setVisible(true);
 	}
 }

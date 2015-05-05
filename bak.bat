15240a5a7b36bbb1a194650438a77472b496ca6c
JafeChang <jafechang@gmail.com>
2015-05-05 14:09:18 +0800

    1.0.150505
    +json

--- bak.bat
+++ bak.bat
@@ -7,7 +7,7 @@ set backupDir=F:\.backup.jafe.info
 set dDir=%~d0
 set fDir=%~p0
 set daytime=%date:~0,4%%date:~5,2%%date:~8,2%-%time:~0,2%%time:~3,2%%time:~6,2%
-set destDir=%backupDir%\%dDir:~0,1%%fDir%%daytime%
+set destDir="%backupDir%\%dDir:~0,1%%fDir%%daytime%"
 if not exist %destDir% (
 	mkdir %destDir%
 	)
@@ -42,6 +42,7 @@ if exist *.html copy  *.html %destDir%
 if exist *.css copy  *.css %destDir% 
 if exist *.jsp copy  *.jsp %destDir% 
 if exist *.c copy  *.c %destDir% 
+if exist *.json copy  *.json %destDir%
 
 echo esc.. 
 echo 5
